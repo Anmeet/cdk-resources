7b1a67c07030f990ae33df15edcdf301ff1ef88c
Amit <anmeet619@gmail.com>
2024-08-09 14:18:09 +1000

    AWS Cognito implemented for login to api gateway

---
+++ space-finder/test/AuthService.ts
@@ -0,0 +1,50 @@
+import { fetchAuthSession, signIn, SignInOutput } from '@aws-amplify/auth'
+import { Amplify } from 'aws-amplify'
+import AuthStack from '../outputs.json'
+import { CognitoIdentityClient } from '@aws-sdk/client-cognito-identity'
+import { fromCognitoIdentityPool } from '@aws-sdk/credential-providers'
+
+const awsRegions = 'us-east-1'
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: AuthStack.SpaceUserPoolId,
+      userPoolClientId: AuthStack.SpaceUserPoolClientId,
+      identityPoolId: AuthStack.SpaceIdentityPoolId,
+    },
+  },
+})
+export class AuthService {
+  public async login(userName: string, password: string) {
+    const signInOutput: SignInOutput = await signIn({
+      username: userName,
+      password: password,
+      options: {
+        authFlowType: 'USER_PASSWORD_AUTH',
+      },
+    })
+
+    return signInOutput
+  }
+
+  public async getIdToken() {
+    const authSession = await fetchAuthSession()
+    return authSession.tokens.idToken?.toString()
+  }
+
+  public async generateTemporaryCredentials() {
+    const idToken = await this.getIdToken()
+    const cognitoIdentityPool = `cognito-idp.${awsRegions}.amazonaws.com/${AuthStack.SpaceUserPoolId}`
+    const cognitoIdentity = new CognitoIdentityClient({
+      credentials: fromCognitoIdentityPool({
+        identityPoolId: AuthStack.SpaceIdentityPoolId,
+        logins: {
+          [cognitoIdentityPool]: idToken,
+        },
+      }),
+    })
+    const credentials = await cognitoIdentity.config.credentials()
+    return credentials
+  }
+}
